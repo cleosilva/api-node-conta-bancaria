c620716a87ea40d10d08ab19ffdff8ebe3a55831
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2022-05-31 12:11:40 -0300

    Implementando método GET de busca da conta bancária por CPF

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const customers = [];
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
+    // método some usado para retornar existe ou não existe
     const customerAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf);
 
@@ -37,5 +38,14 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 
 });
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    return response.json(customer.statement);
+
+})
 
 app.listen(3333);
